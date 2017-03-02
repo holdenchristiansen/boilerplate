a1d1738a232ecdd07f97606d812dcce9884b6ecb
Holden Christiansen <holden16@icloud.com>
2017-03-02 10:45:01 -0600

    Gruntfile, doctype

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,47 +22,67 @@ module.exports = function(grunt) {
     watch: {
       css: {
         files: ['Gruntfile.js', 'src/css/*', 'src/js/*', 'src/js/vendor/*'],
-        tasks: ['less', 'jshint', 'uglify'],
+        tasks: ['less:dev', 'jshint', 'uglify:dev'],
       }
     },
 
     less: {
-      files: {
-        src: ['src/css/app.less'],
-        dest: 'build/app.min.css',
+      dev: {
+        files: {
+          'src/build/app.css': 'src/css/app.less',
+        },
+        options: {
+          compress: false,
+        },
+      },
+      prod: {
+        files: {
+          'src/build/app.css': 'src/css/app.less',
+        },
         options: {
           compress: true,
-        }
+        },
       }
     },
+
 
     jshint: {
       all: ['Gruntfile.js', 'src/js/*.js']
     },
 
     uglify: {
-      files: {
+      dev: {
         src: [
+          'src/js/vendor/jquery-3.1.1.js',
           'src/js/vendor/*.js',
           'src/js/*.js'
         ],
-        dest: 'build/app.min.js'
+        dest: 'src/build/app.js',
+        options: {
+          beautify: true,
+        }
+      },
+      prod: {
+        files: {
+          'src/build/app.js': 'src/js/app.js',
+        },
       }
-    }
+    },
 
   });
 
+
   grunt.registerTask('default', [
-    'less',
+    'less:dev',
     'jshint',
-    'uglify',
+    'uglify:dev',
     'connect:dev',
     'watch:css',
   ]);
 
   grunt.registerTask('prod', [
-    'less',
-    'uglify',
+    'less:prod',
+    'uglify:prod',
     'connect:prod',
   ]);
 };
